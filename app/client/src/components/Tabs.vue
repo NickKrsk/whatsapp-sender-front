5c1fc1583c558a82d546515fad4c35023e452272
NickKrsk <ggd43@rambler.ru>
2021-10-27 22:23:56 +0700

    lets go!

---
+++ app/client/src/components/Tabs.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <q-tabs v-model="tab" class="bg-primary text-white shadow-5">
+      <q-route-tab name="tasks" icon="task" label="Задания" to="/"></q-route-tab>
+      <q-route-tab name="lists" icon="list" label="Списки рассылки" to="/lists"></q-route-tab>
+    </q-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  // name: 'ComponentName',
+  setup() {
+    return {
+    };
+  },
+};
+</script>
---
+++ app/client/src/pages/Lists.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h5>Тут будут списки рассылки</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  // name: 'ComponentName',
+  setup() {
+    return {};
+  },
+};
+</script>
